7e208a8875483a09f9b55dcf2df3cfd925f443cd
aneoapple <43287253+aneoapple@users.noreply.github.com>
2025-09-08 19:25:06 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,29 @@
-// network-first e IGNORA chamadas ao Apps Script
-const CACHE_NAME = 'estoque-headset-v1';
-const APP_SHELL = ['./','./index.html','./logo.png'];
+const CACHE = "estoque-headset-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(APP_SHELL)));
-  self.skipWaiting();
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME ? caches.delete(k) : null))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  // deixa a API do Apps Script passar direto
-  if (url.hostname.includes('script.google.com')) return;
-
-  e.respondWith(
-    fetch(e.request)
-      .then(resp => { caches.open(CACHE_NAME).then(c => c.put(e.request, resp.clone())).catch(()=>{}); return resp; })
-      .catch(() => caches.match(e.request))
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (ASSETS.some(a => url.pathname.endsWith(a.replace("./","/")))) {
+    e.respondWith(
+      caches.match(e.request).then(resp => resp || fetch(e.request))
+    );
+  }
 });
