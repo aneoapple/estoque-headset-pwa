e3611623e716d106cc612768867660422379bf85
aneoapple <43287253+aneoapple@users.noreply.github.com>
2025-09-08 14:10:17 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,8 @@
-const CACHE = "estoque-headset-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open('eh-cache').then(c=>c.addAll(['./','./index.html'])));
 });
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(resp=>resp || fetch(e.request))
   );
-});
-
-self.addEventListener("fetch", (e) => {
-  const url = new URL(e.request.url);
-  if (ASSETS.some(a => url.pathname.endsWith(a.replace("./","/")))) {
-    e.respondWith(
-      caches.match(e.request).then(resp => resp || fetch(e.request))
-    );
-  }
 });
